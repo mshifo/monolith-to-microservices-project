74c61b8860cf50ef66b17ef3ba49b82944042cf8
Mahmoud Elshafaey <mahmoud.elshafaey@linkdev.com>
2023-01-15 23:31:12 +0200

    deployment files

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://aed10e65a21544127aca654d55ca47b9-1729447287.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
